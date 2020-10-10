f54cb1b185c8caac1252f3461ddf26a0ca699ef1
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-10 13:25:53 -0500

    add auth to me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,7 +4,18 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        me: async (parent, args, context) => {
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('savedBooks')
+          
+              return userData;
+            }
+          
+            throw new AuthenticationError('Not logged in');
+          }
+          
     },
     Mutation: {
         addUser: async (parent, args) => {
--- server/server.js
+++ server/server.js
@@ -5,13 +5,16 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
