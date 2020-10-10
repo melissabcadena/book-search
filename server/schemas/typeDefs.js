d4bac3040fff0f12adcfba6128f69de8e54d09ed
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-10-09 22:12:11 -0500

    add typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,46 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}  
+
+type Book {
+    bookId: Int
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
   }
+
+input BookInput {
+    author: [String]
+    title
+    bookId
+    image
+    link
+}
+
+type Mutation {
+    login(username: String!, password: String!): Auth
+    addUsername(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: Int): User
+}
 `;
 
 // export the typeDefs
